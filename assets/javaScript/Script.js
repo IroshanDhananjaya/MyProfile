1d336df763b224db5c2a5b77c2ddcb720698a347
Dhananjaya <iroshandhananjaya0322@gmail.com>
2022-03-09 13:12:20 +0530

    add smooth scroll

--- assets/javaScript/Script.js
+++ assets/javaScript/Script.js
@@ -8,3 +8,54 @@ toggleMenu.addEventListener('click',()=>{
 closeMenu.addEventListener('click',()=>{
     navMenu.classList.remove('show')
 })
+
+window.addEventListener('scroll',()=>{
+    let content=document.querySelector('.about-section');
+    let contentPosition=content.getBoundingClientRect().top;
+    let screenPostion=window.innerHeight;
+    if(contentPosition<screenPostion){
+       content.classList.add('active')
+    }else {
+        content.classList.remove('active')
+    }
+});
+window.addEventListener('scroll',()=>{
+    let content=document.querySelector('.education-area');
+    let contentPosition=content.getBoundingClientRect().top;
+    let screenPostion=window.innerHeight;
+    if(contentPosition<screenPostion){
+        content.classList.add('active1')
+    }else {
+        content.classList.remove('active1')
+    }
+});
+window.addEventListener('scroll',()=>{
+    let content=document.querySelector('.skill-area');
+    let contentPosition=content.getBoundingClientRect().top;
+    let screenPostion=window.innerHeight;
+    if(contentPosition<screenPostion){
+        content.classList.add('active2')
+    }else {
+        content.classList.remove('active2')
+    }
+});
+window.addEventListener('scroll',()=>{
+    let content=document.querySelector('.service-area');
+    let contentPosition=content.getBoundingClientRect().top;
+    let screenPostion=window.innerHeight;
+    if(contentPosition<screenPostion){
+        content.classList.add('active3')
+    }else {
+        content.classList.remove('active3')
+    }
+});
+window.addEventListener('scroll',()=>{
+    let content=document.querySelector('.projects-area');
+    let contentPosition=content.getBoundingClientRect().top;
+    let screenPostion=window.innerHeight;
+    if(contentPosition<screenPostion){
+        content.classList.add('active4')
+    }else {
+        content.classList.remove('active4')
+    }
+});
